f05f6bfef09b5fc6a12b10e404a86422417650e4
Suryansh <suryanshrao01@gmail.com>
2024-07-16 18:15:45 +0530

    Completed the transaction doing part.

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { Account } = require("../db");
+const { authMiddleware } = require("../middleware");
+const { default: mongoose } = require("mongoose");
 
 const router = express.Router();
 
@@ -11,6 +13,39 @@ router.get("/balance", authMiddleware, async(req,res) => {
     res.json({
         balance : account.balance
     })
+});
+
+router.post("/transfer", authMiddleware, async(req,res) => {
+    const session = await mongoose.startSession();
+
+    session.startTransaction();
+    const { amount, to } = req.body;
+
+    const account = await Account.findOne({ userId: req.userId}).session(session);
+
+    if(!account || account.balance < amount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            message : "Insufficient Balance"
+        });
+    }
+
+    const toAccount = await Account.findOne({userId : to}).session(session);
+
+    if(!toAccount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            message : "No such account exists."
+        });
+    }
+
+    await Account.updateOne({userId:req.userId}, {$inc : {balance : -amount}}).session(session);
+    await Account.updateOne({userId: to}, {$inc : {balance : amount}}).session(session);
+
+    await session.commitTransaction();
+    res.json({
+        message : "Transaction Successful"
+    });
 });
 
 module.exports = router;

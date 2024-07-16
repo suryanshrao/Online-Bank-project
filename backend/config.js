ffdec3d94d1043251e8bd4d6235e70931a04d592
Suryansh <suryanshrao01@gmail.com>
2024-07-16 22:07:33 +0530

    Changes in db and index

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 const JWT_SECRET = "SuryanshRao"
 
-module.exports = JWT_SECRET
+module.exports = {JWT_SECRET}
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const cors = require('cors');
 const bodyParser = require("body-parser");
+const mainRouter = require("./routes/index"); 
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
-
-const mainRouter = require("./routes/index");
-
+app.use(express.json());
 
 app.use("/api/v1", mainRouter);
 

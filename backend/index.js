f044735de9905c7b34b43ee4f358b753b78e0b02
Suryansh <suryanshrao01@gmail.com>
2024-07-16 23:45:54 +0530

    k

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require('cors');
-const bodyParser = require("body-parser");
 const mainRouter = require("./routes/index"); 
 
 const app = express();

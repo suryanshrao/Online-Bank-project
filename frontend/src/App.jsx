049962e7882252991e66f2e1fbc296bc57c804d6
Suryansh <suryanshrao01@gmail.com>
2024-07-18 17:37:41 +0530

    Intializing Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
-  
-
   return (
-    <div>
-      Hello World
-    </div>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/send" element={<Send />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;

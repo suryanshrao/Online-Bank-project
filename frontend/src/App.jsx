7024c00e0d72506eeee3448dc65e3f0a5ea9b30a
Suryansh <suryanshrao01@gmail.com>
2024-07-20 03:21:21 +0530

    project-almost complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/Transaction";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-        <Route path="/signup" element={<SignUp />}></Route>
-        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/signin" element={<Signin />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
-        <Route path="/send" element={<Send />}></Route>
+        <Route path="/transaction" element={<SendMoney />}></Route>
         </Routes>
       </BrowserRouter>
     </>
